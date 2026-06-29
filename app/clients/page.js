ec6b91e12b25b9870a4645b906b64652099beb4f
davidemarziani <davide.marziani@gmail.com>
2026-06-29 11:31:30 +0200

    Force clients page to load fresh data

--- app/clients/page.js
+++ app/clients/page.js
@@ -2,6 +2,9 @@ import Link from "next/link";
 import ClientsTable from "../../components/ClientsTable";
 import { prisma } from "../../lib/prisma";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function ClientsPage() {
   const clients = await prisma.client.findMany({
     orderBy: {
